2b97a065d7b159e64b22d6e995abb609d5f97819
gustavfaustino <gustav.faustino107@gmail.com>
2024-04-11 19:44:24 -0300

    Segunda aula da DevWeek Santander com a DIO

--- src/main/java/me/dio/sdw24/adapters/in/AskChampionRestController.java
+++ src/main/java/me/dio/sdw24/adapters/in/AskChampionRestController.java
@@ -2,11 +2,8 @@ package me.dio.sdw24.adapters.in;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dio.sdw24.application.AskChampionUseCase;
-import me.dio.sdw24.application.ListChampionsUseCase;
-import me.dio.sdw24.domain.model.Champion;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Tag(name = "Campeões", description = "Endpoints do domínio de campeões do LoL.")
 @RestController
